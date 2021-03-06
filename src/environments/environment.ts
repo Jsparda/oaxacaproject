5bd7978b8cb03c3ae24707d75da7fa68729d9f88
Hector JS <miguel.lin123@gmail.com>
2021-03-06 14:11:59 -0600

    Se aplicaron cambios para la integracion de la base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDbsM-3eYO3ki84LAAtCZkwjFJY08T5Ve4",
+    authDomain: "dexings-3f89d.firebaseapp.com",
+    projectId: "dexings-3f89d",
+    storageBucket: "dexings-3f89d.appspot.com",
+    messagingSenderId: "976181416236",
+    appId: "1:976181416236:web:bab652a7af631061759c89"
+  }
 };
 
 /*
